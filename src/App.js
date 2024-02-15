6a4acf9684c1f64dde4a71ac0079db9f94b152f0
aryanjnv <aryangpjnv188@gmail.com>
2024-02-15 22:27:23 +0530

    Cart Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Store from './Components/Store/Store';
+import Banner from './Components/Banner/Banner';
+import Footer from './Components/Footer/Footer';
+
+import { useState } from 'react';
+import Cart from './Components/Modal/Cart';
 
 function App() {
+  const [cartIsShown,setCartIsShown]=useState(false)
+
+  const showCartHandler=()=>{
+    setCartIsShown(true)
+  }
+  const hideCartHandler=()=>{
+    setCartIsShown(false)
+    
+  }
   return (
     <div>
-      <Navbar/>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Navbar onClick={showCartHandler}/>
+      <Banner/>
       <Store/>
+      <Footer/>
+      {/* <Modal/> */}
     
     </div>
   );
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Banner.module.css'
+
+const Banner = () => {
+  return (
+    <div className={classes.container}>
+        <h1 className={classes.heading}>The Generics</h1>
+    </div>
+  )
+}
+
+export default Banner
