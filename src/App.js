6562850ef537b3f00a926f77c1fc5669593bde1d
aryanjnv <aryangpjnv188@gmail.com>
2024-02-21 17:45:17 +0530

    Protecting the product page

--- src/App.js
+++ src/App.js
@@ -102,9 +102,9 @@ function App() {
         <Navbar onClick={showCartHandler}/>
         
         <Routes>
-          <Route path='/' element={<Store productsArr={productsArr}/>}/>
+          <Route path='/store' element={<Store productsArr={productsArr}/>}/>
           <Route path='/about' element={<About/>}/>
-          <Route path='/home' element={<Home/>}/>
+          <Route path='/' element={<Home/>}/>
           <Route path='/contact' element={<ContactUs/>}/>
           <Route path='/products/:productId' element={<ProductDetail productsArr={productsArr}/>} exact/>
           <Route path="/login" element={<Login/>}/>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,68 +1,45 @@
-import React, { useContext, useState } from "react";
-import classes from "./Navbar.module.css";
-import CartContext from "../Context/cart-context";
-import { Link, NavLink, useNavigate } from "react-router-dom";
-import AuthContext from "../../Auth-Context/auth-context";
+import React, { useContext, useState } from 'react'
+import classes from './Navbar.module.css'
+import CartContext from '../Context/cart-context'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
+import AuthContext from '../../Auth-Context/auth-context'
+
 
 const Navbar = (props) => {
-  const navigate = useNavigate();
-  const [show, setShow] = useState(false);
-  const cartContext = useContext(CartContext);
-  let quantity = cartContext.items.length;
+    const navigate=useNavigate()
+    const [show,setShow]=useState(false)
+    const cartContext=useContext(CartContext)
+    let quantity=cartContext.items.length
 
-  const authcontext = useContext(AuthContext);
+    const authcontext=useContext(AuthContext)
 
-  const isLoggedIn = authcontext.isLoggedIn;
+    const isLoggedIn=authcontext.isLoggedIn
 
-  const logOutHandler = () => {
-    navigate("/login");
-    authcontext.logout();
-  };
+    const logOutHandler=()=>{
+       navigate('/login')
+       authcontext.logout()
+    }
   return (
     <>
-      <nav className={classes.nav}>
+    <nav className={classes.nav}>
         <ul className={classes.list}>
-          <NavLink to="/home">
-            {" "}
-            <li>Home</li>
-          </NavLink>
-          <NavLink to="/">
-            <li>Store</li>
-          </NavLink>
-          <NavLink to="/about">
-            <li>About</li>
-          </NavLink>
-          <NavLink to="/contact">
-            <li>Contact </li>
-          </NavLink>
-          {isLoggedIn && (
-            <NavLink to="/password">
-              <li>Profile</li>
-            </NavLink>
-          )}
+           <NavLink to='/'> <li>Home</li></NavLink>
+        {  isLoggedIn ? <NavLink to='/store'><li>Store</li></NavLink> :<NavLink to='/login'><li>Store</li></NavLink>}
+           <NavLink to='/about'><li>About</li></NavLink>
+          <NavLink to='/contact'><li>Contact </li></NavLink> 
+         {isLoggedIn && <NavLink to='/password'><li>Profile</li></NavLink>}
         </ul>
         <div className={classes.cart}>
-          {!isLoggedIn && (
-            <NavLink to="/login">
-              <h3 className={classes.login}>Login</h3>
-            </NavLink>
-          )}
-          {isLoggedIn && (
-            <h3 onClick={logOutHandler} className={classes.login}>
-              Logout
-            </h3>
-          )}
-
-          {isLoggedIn && (
-            <button className={classes.button} onClick={props.onClick}>
-              Cart
-            </button>
-          )}
-          {isLoggedIn && <p className={classes.p}>{quantity}</p>}
+          {!isLoggedIn && <NavLink to='/login'><h3 className={classes.login}>Login</h3></NavLink> } 
+           {isLoggedIn &&    <h3 onClick={logOutHandler} className={classes.login}>Logout</h3>}
+          
+           {isLoggedIn && <button className={classes.button} onClick={props.onClick}>Cart</button>}
+           {isLoggedIn && <p className={classes.p}>{quantity }</p>}
         </div>
-      </nav>
+    </nav>
+   
     </>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
